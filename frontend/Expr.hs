ca7637c18ad102565a933f13bdb0fdca0803b6de
Overmind JIANG <p90eri@gmail.com>
2016-05-18 22:24:09 +0800

    feat(frontend): add a simple parser.

--- frontend/Expr.hs
+++ frontend/Expr.hs
@@ -1,3 +1,57 @@
 module Expr where
 
-import Text.Parsec
+import Data.Functor.Foldable
+import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
+
+type Name = String
+
+prettyExpr :: Expr -> Doc
+prettyExpr = pretty
+
+data ExprF f
+  = AddF f f
+  | SubF f f
+  | LitF Int
+  | LessThanF f f
+  | VarF Name
+  | LetF [(Name, f)] f
+  | BeginF [f]
+  | IfF f f f
+  | ApplyF f [f]
+  | LambdaF [Name] f
+  deriving (Show, Eq, Ord, Functor)
+
+type Expr = Fix ExprF
+
+pattern Add x y = Fix (AddF x y)
+pattern Sub x y = Fix (SubF x y)
+pattern LessThan x y = Fix (LessThanF x y)
+pattern Var x = Fix (VarF x)
+pattern Let bs x = Fix (LetF bs x)
+pattern Lit x = Fix (LitF x)
+pattern If c t f = Fix (IfF c t f)
+pattern Begin xs = Fix (BeginF xs)
+pattern Apply f xs = Fix (ApplyF f xs)
+pattern Lambda vs x = Fix (LambdaF vs x)
+
+instance Pretty Expr where
+  pretty = cata go
+    where
+      go = \case
+        AddF a b -> parens (a <+> text "+" <+> b)
+        SubF a b -> parens (a <+> text "-" <+> b)
+        LessThanF a b -> parens (a <+> text "<" <+> b)
+        VarF x -> pretty x
+        LetF bs x -> text "let" <$$> indent 2 (vsep . map pBinding $ bs) <$$> text "in" <+> x
+        LitF i -> pretty i
+        BeginF xs -> text "{" <$$> indent 2 (vsep xs) <$$> text "}"
+        IfF c t f -> parens (text "if" <+> c
+          <+> text "then" <+> t
+          <+> text "else" <+> f)
+        ApplyF f xs -> f <> args xs
+        LambdaF vs x -> parens (text "\\" <> args vs <+> text "->" <+> x)
+
+      args :: Pretty a => [a] -> Doc
+      args = parens . cat . punctuate comma . map pretty
+
+      pBinding (name, x) = pretty name <+> equals <+> x
---
+++ frontend/Parser.hs
@@ -0,0 +1,83 @@
+module Parser
+  ( parseExpr
+  ) where
+
+import Text.Parsec
+import Data.Functor.Identity
+import Text.Parsec.String (Parser)
+import Text.Parsec.Language (javaStyle)
+import Text.Parsec.Expr
+import qualified Text.Parsec.Token as P
+
+import Expr
+
+parseExpr :: String -> Expr
+parseExpr source = case parse pExpr "Expr" source of
+  Left e -> error $ "parseExpr: " ++ show e
+  Right r -> r
+
+pName :: Parser Name
+pName = ident <?> "identifier"
+
+pLit :: Parser Expr
+pLit = Lit . fromInteger <$> integer <?> "int-literal"
+
+pTerm :: Parser Expr
+pTerm = buildExpressionParser table term <?> "compound-term"
+  where
+    term = parens pExpr <|> pLit <|> (Var <$> pName) <?> "term"
+    table = [ [Postfix funcall]
+            , [binary "+" Add AssocLeft, binary "-" Sub AssocLeft]
+            , [binary "<" LessThan AssocLeft]
+            ]
+    binary opName op = Infix (reservedOp opName *> pure op)
+    funcall = flip Apply <$> parens (commaSep pExpr)
+
+pExpr :: Parser Expr
+pExpr = pIf <|> pLet <|> pSeq <|> pLambda <|> pTerm <?> "expr"
+  where
+    pIf = If
+      <$> (reserved "if" *> pExpr)
+      <*> (reserved "then" *> pExpr)
+      <*> (reserved "else" *> pExpr)
+
+    pLet = Let
+      <$> (reserved "let" *> many1 pBinding)
+      <*> (reserved "in" *> pExpr)
+
+    pLambda = Lambda
+      <$> (reservedOp "\\" *> parens (commaSep pName))
+      <*> (reservedOp "->" *> pExpr)
+
+    pSeq = Begin
+      <$> braces (many pExpr)
+
+    pBinding = (,) <$> pName <*> (reservedOp "=" *> pExpr)
+
+-- Tokens
+
+lexer :: P.GenTokenParser String u Identity
+lexer = P.makeTokenParser (javaStyle { P.reservedNames = words "if then else let in"
+                                     , P.reservedOpNames = words "+ - < = \\ ->"
+                                     })
+
+parens :: forall u a. ParsecT String u Identity a -> ParsecT String u Identity a
+parens = P.parens lexer
+
+braces :: forall u a. ParsecT String u Identity a -> ParsecT String u Identity a
+braces = P.braces lexer
+
+ident :: forall u. ParsecT String u Identity String
+ident = P.identifier lexer
+
+reserved :: forall u. String -> ParsecT String u Identity ()
+reserved = P.reserved lexer
+
+reservedOp :: forall u. String -> ParsecT String u Identity ()
+reservedOp = P.reservedOp lexer
+
+integer :: forall u. ParsecT String u Identity Integer
+integer = P.integer lexer
+
+commaSep :: forall u a. ParsecT String u Identity a -> ParsecT String u Identity [a]
+commaSep = P.commaSep lexer
